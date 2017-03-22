22146cec0a814dee3c28cb39cc6e97381871b814
triccetti <triccetti@cettiworks.com>
2017-03-22 14:08:07 -0700

    logs, and refrence

--- src/main/java/com/tayloristic/nokamod/Refrence.java
+++ src/main/java/com/tayloristic/nokamod/Refrence.java
@@ -4,7 +4,7 @@ public class Refrence {
 	public static final String MOD_ID = "nokamod";
 	public static final String NAME = "Noka Mod";
 	public static final String VERSION = "1.0";
-	public static final String ACCEPTED_VERSIONS = "[1.11.2]";
+	public static final String ACCEPTED_VERSIONS = "[1.11,1.11.2]";
 	
 	public static final String CLIENT_PROXY = "com.tayloristic.nokamod.proxy.ClientProxy";
 	public static final String SERVER_PROXY = "com.tayloristic.nokamod.proxy.ServerProxy";
